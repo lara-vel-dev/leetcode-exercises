85666a90eb12fd1070160d1c7e36235cca535173
Daniela Lara <laradni@icloud.com>
2025-01-06 18:49:27 -0600

    (Add) Running Sum of 1D Array Solution

--- beginners-guide/01-runningsums.py
+++ beginners-guide/01-runningsums.py
@@ -7,7 +7,6 @@ Given an array nums. We define a running sum of an array as runningSum[i] = sum(
 Return the running sum of nums.
 
 Example:
-
 Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
