42e59aab0031b4730e1aa97442b3e856f87eea36
Daniela Lara <laradni@icloud.com>
2025-01-07 21:15:56 -0600

    (Add) Palindrome Number Solution

--- easy-problems/03-palindrome.py
+++ easy-problems/03-palindrome.py
@@ -24,20 +24,20 @@ Constraints:
 # Solution using strings
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1] 
+        return str(x) == str(x)[::-1]
 
 # Solution without using strings
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return False 
-            
+            return False
+
         reversed_num = 0
         original_num = x
-        
+
         while x != 0:
             digit = x % 10
             reversed_num = reversed_num * 10 + digit
             x //= 10
-        
+
         return original_num == reversed_num
